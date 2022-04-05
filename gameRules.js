0060487766b5b24abad51f66aebac69ff1d86f6e
carlotamburin <carlo.tamburin@gmail.com>
2022-04-05 22:24:05 +0200

    Popravljena logika kretanja u turnu

--- gameRules.js
+++ gameRules.js
@@ -27,7 +27,6 @@ export function takeSeat(...players) {
     Game.tableSet[players[i].position] = players[i];
     //console.log(players[i].position + ":" + players[i].team);
   }
-  console.log(Game.tableSet);
 }
 
 function lastTurnWinner(player1, player2, player3, player4) {
@@ -42,33 +41,35 @@ function lastTurnWinner(player1, player2, player3, player4) {
   }
 }
 
-export function playTurn(player1, player2, player3, player4) {
+export function playTurn(...players) {
   let cardsThrown = 0;
-  let lastWinner = lastTurnWinner(...arguments);
-
+  let lastWinner = lastTurnWinner(...players);
   const { hand: lastTurnWinnerHand } = lastWinner;
-
-  //while petlja
 
   lastWinner.currentCard = lastWinner
     .playCard(sample(lastTurnWinnerHand))
     .shift();
   Game.winningSuit = lastWinner.currentCard.suit;
+  cardsThrown += 1;
+  console.log(lastWinner.currentCard);
 
-  let secondPlayer = nextPlayer(Game.lastPlayer);
-  Game.lastPlayer = secondPlayer;
-  secondPlayer.currentCard = secondPlayer
-    .playCard(sample(secondPlayer.hand))
-    .shift();
+  while (cardsThrown != 4) {
+    let secondPlayer = nextPlayer(Game.lastPlayer);
+    secondPlayer.currentCard = secondPlayer
+      .playCard(sample(secondPlayer.hand))
+      .shift();
+    cardsThrown += 1;
+    console.log(secondPlayer.currentCard);
 
-  winningCard(lastWinner, secondPlayer); //Pobjednik ostaje
+    lastWinner = winningCard(lastWinner, secondPlayer); //Pobjednik ostaje
+    Game.lastPlayer = secondPlayer;
+  }
+  console.log("Pobjednicka karta je: ", lastWinner.currentCard);
 }
 
 function winningCard(player1, player2) {
   const { currentCard: player1Card } = player1;
   const { currentCard: player2Card } = player2;
-
-  if (player1.team == player2.team) return;
 
   if (player1Card.suit == Game.winningSuit) {
     if (player2Card.suit != player1Card.suit) {
@@ -97,9 +98,9 @@ function nextPlayer(lastPlayer) {
   let indexOfLastPLayer = table.indexOf(lastPlayerPosition);
   let val = table[(((indexOfLastPLayer + 1) % tableLen) + tableLen) % tableLen];
 
-  console.log("The next player position " + val);
-  console.log("The next player team: " + TABLE_POSITION[val]);
-  console.log("The last player position " + indexOfLastPLayer);
+  // console.log("The next player position " + val);
+  // console.log("The next player team: " + TABLE_POSITION[val]);
+  // console.log("The last player position " + indexOfLastPLayer);
 
   return Game.tableSet[val];
 }
